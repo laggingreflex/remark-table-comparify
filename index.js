fd4b1eaa7944a33d8fab6031fdded69542d2285a
laggingreflex <laggingreflex@gmail.com>
2022-01-21 07:38:36 +0530

    feat: modify result, defaults operator

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+import { remark } from 'remark';
+import remarkGfm from 'remark-gfm';
 import { visit } from 'unist-util-visit';
+import { toMarkdown } from 'mdast-util-to-markdown';
+import { gfmToMarkdown } from 'mdast-util-gfm'
 import _ from './utils.js';
 const debug = _.debug();
 
@@ -11,6 +15,7 @@ export const defaults = {
   },
   resultSymbol: '=',
   modifierSeparator: ':',
+  defaultSeparator: '?',
 };
 
 /**
@@ -21,7 +26,7 @@ export const defaults = {
 export default function tableComparify(opts) {
   return (tree, file) => {
     const state = file.data.tableComparify = {};
-    visit(tree, 'table', table => onTable({ table, file, state, opts }));
+    visit(tree, 'table', node => onTable({ node, file, state, opts }));
     if (!state.found) {
       file.message(`Couldn't find a table (be sure to use remark-gfm before)`);
     } else if (!state.altered) {
@@ -32,68 +37,70 @@ export default function tableComparify(opts) {
   }
 }
 
-function onTable({ table, file, state, opts }) {
-  state.found = true;
-  debug({ table, file, state, opts });
+/**
+ * Modify a Markdown string containing a table and returns the modified output
+ * @param {string} input
+ * @param {object} opts
+ * @returns {string} output
+ */
+export async function modify(input, opts) {
+  const modifications = [];
+  await remark().use(remarkGfm).use(opts => (tree, file) => visit(tree, 'table', original => {
+    const modified = onTable({ node: original, file });
+    modifications.push(modified);
+  })).process(input);
+  const originals = modifications.map(modification =>
+    input.substring(
+      modification.position.start.offset,
+      modification.position.end.offset,
+    )
+  );
+  for (let i = 0; i < originals.length; i++) {
+    const original = originals[i];
+    const modified = modifications[i];
+    let string = toMarkdown(modified, { extensions: [gfmToMarkdown()] });
+    string = string.replace(/\n$/, '');
+    input = input.replace(original, string);
+  }
+  return input;
+}
 
+function onTable({ node, file, state, opts }) {
+  state && (state.found = true);
   const operations = opts?.operations || defaults.operations;
   const operationSymbols = Object.keys(operations);
-  // const operationSymbolsRegex = new RegExp(operationSymbols.join('|'));
   const operationsQueue = operationSymbols.reduce((ops, op) => ({ ...ops, [op]: [] }), {});
   let resultColI;
   let hasAtleastOneOp
-  const [headerRow, ...rows] = table.children;
-  debug({ headerRow });
+  const [headerRow, ...rows] = node.children;
   for (let i = 0; i < headerRow.children.length; i++) {
     const column = headerRow.children[i];
     const [{ value: header }] = column.children;
-    const modifier = {};
-    debug({ header });
-    for (const op in operationsQueue) {
-      if (_.startsWith(header, op)) {
-        let modifier;
-        if (header.includes(':')) {
-          let [, modifierString] = header.split(':');
-          let operation, number;
-          const opRx = '\\' + operationSymbols.join('|\\');
-          const regexpString = `(${opRx})([0-9]+)`;
-          const regExp = new RegExp(regexpString, 'i');
-          [, operation, number] = modifierString.match(regExp) || [];
-          number = Number(number);
-          debug({ header, modifier: modifierString, opRx, regexpString, regExp });
-          if (!operation) throw new Error(`Couldn't extract operation from: "${header}"`);
-          if (!number) throw new Error(`Couldn't extract number from: "${header}"`);
-          modifier = { operation, number };
-        }
-        operationsQueue[op].push({ i, header, modifier });
-        hasAtleastOneOp = true;
-      }
+    const meta = getMeta(header, opts);
+    if (meta.operation) {
+      operationsQueue[meta.operation].push({ i, header, ...meta });
+      hasAtleastOneOp = true;
     }
-    if (_.startsWith(header, '=')) resultColI = i;
-
+    if (_.startsWith(header, '=')) {
+      if (typeof resultColI !== typeof undefined) file.message(`Multiple "=result" columns, picking last one`);
+      resultColI = i;
+    }
   }
   if (typeof resultColI === 'undefined') {
-    const error = `Couldn't find "result" column — a column that starts with a '='`
-    // throw new Error(error);
-    // console.warn(error);
-    file.message(error)
+    file.message(`Couldn't find "result" column — a column that starts with a '='`);
     return;
   }
   if (!hasAtleastOneOp) {
-    const error = `Couldn't find any column that contributes to result. Such a column would start with a mathematical operator: + - * /`
-    file.message(error); // file.fail(error)
+    file.message(`Couldn't find any column that contributes to result. Such a column would start with a mathematical operator: + - * /`);
     return;
   }
   for (const row of rows) try {
-    debug(row);
     let newResult = 0;
     for (const op in operationsQueue) {
-      for (const { i, header, modifier } of operationsQueue[op]) {
+      for (const { i, header, modifier, defaultValue } of operationsQueue[op]) {
         const column = row.children[i]
-        debug({ header, row, i, column, modifier });
-        let val = column.children?. [0]?.value ?? 0;
+        let val = column.children?. [0]?.value ?? defaultValue ?? 0;
         val = Number(val);
-        debug({ header, val });
         if (modifier) {
           val = operations[modifier.operation](val, modifier.number);
         }
@@ -101,12 +108,8 @@ function onTable({ table, file, state, opts }) {
       }
     }
     let resultCol = row.children[resultColI];
-    debug({ resultColI, resultCol });
-    newResult = _.toFixed(newResult);
-    debug({ resultColI, resultCol, newResult })
     if (resultCol?.children?. [0]) {
       resultCol.children[0].value = newResult
-      debug(resultCol.children)
     } else {
       if (!resultCol) {
         for (let i = row.children.length; i <= resultColI; i++) {
@@ -120,8 +123,59 @@ function onTable({ table, file, state, opts }) {
     debug(row);
     throw error;
   }
+
+  const results = rows.map(row => row.children[resultColI].children[0]);
+  const min = Math.min(...results.map(r => r.value)) ?? 1;
+  const resultHeader = headerRow.children[resultColI].children[0];
+  const headerMeta = getMeta(resultHeader.value, { ...opts, min });
+  if (headerMeta.modifier) {
+    for (const result of results) {
+      result.value = operations[headerMeta.modifier.operation](result.value, headerMeta.modifier.number);
+    }
+  }
+
+  for (const result of results) {
+    result.value = _.toFixed(result.value);
+  }
+
   _.sort(rows, r => r.children[resultColI].children[0].value, true);
-  table.children = [headerRow, ...rows];
-  state.altered = true;
-  return table;
+
+  node.children = [headerRow, ...rows];
+  state && (state.altered = true);
+  return node;
+}
+
+
+function getMeta(string, opts) {
+  const operations = opts?.operations || defaults.operations;
+  const operationSymbols = Object.keys(operations);
+  const min = opts?.min ?? -Infinity;
+  const defaultSeparator = opts?.defaultSeparator || defaults.defaultSeparator;
+
+  const meta = {};
+
+  for (const operation in defaults.operations) {
+    if (_.startsWith(string, operation)) {
+      meta.operation = operation;
+    }
+  }
+
+  if (string.includes(':')) {
+    let [, modifierString] = string.split(':');
+    const opRx = '\\' + operationSymbols.join('|\\');
+    const regexpString = `(${opRx})([0-9]+|min)`;
+    const regExp = new RegExp(regexpString, 'i');
+    const modifier = meta.modifier = {};
+    [, modifier.operation, modifier.number] = modifierString.match(regExp) || [];
+    if (modifier.number === 'min') modifier.number = min;
+    modifier.number = Number(modifier.number);
+  }
+
+  if (string.includes(defaultSeparator)) {
+    const regex = new RegExp(`\\${defaultSeparator}(-?[0-9]+)`);
+    // [, meta.defaultValue] = string.match(/\?(-?[0-9]+)/);
+    [, meta.defaultValue] = string.match(regex);
+  }
+
+  return meta;
 }
